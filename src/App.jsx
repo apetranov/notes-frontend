a111beffeb7bab859662486809262dc4ae5c583e
Alexander Petranov <shasho68.5@gmail.com>
2026-07-27 10:48:03 +0300

    Implement basic login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,40 @@ import axios from 'axios'
 import noteService from './services/notes'
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
+import loginService from './services/login';
+import LoginForm from "./components/LoginForm";
+import NoteForm from "./components/NoteForm";
 
 const App = () => {
   const [notes, setNotes] = useState(null);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async event => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+      console.log("user: ", user);
+    } catch {
+      setErrorMessage('wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 500);
+    }
+  }
 
 
   const toggleImportanceOf = (id) => {
@@ -39,8 +67,20 @@ const App = () => {
         setNotes(initialNotes)
       )
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
   if (!notes) return null
   console.log('render', notes.length, 'notes');
+
+  
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
@@ -65,10 +105,104 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
+  // const handleLogin = (event) => {
+  //   event.preventDefault();
+  //   console.log('logging in with', username, password);
+  // }
+
+  // const loginForm = () => (
+  //   <form onSubmit={handleLogin}>
+  //     <div>
+  //       <label>
+  //         username
+  //         <input 
+  //           type="text" 
+  //           value={username}
+  //           onChange={({ target }) => setUsername(target.value)}
+  //         />
+  //       </label>
+  //     </div>
+  //     <div>
+  //       <label>
+  //         password
+  //         <input 
+  //           type="text" 
+  //           value={password}
+  //           onChange={({ target }) => setPassword(target.value)}
+  //         />
+  //       </label>
+  //     </div>
+  //     <button type="submit">login</button>
+  //   </form>
+  // )
+
+  // const noteForm = () => (
+  //   <form onSubmit={addNote}>
+  //     <input value={newNote} onChange={handleNoteChange} />
+  //     <button type="submit">save</button>
+  //   </form>
+  // )
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+{/* 
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input 
+              type="text" 
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input 
+              type="password" 
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form> */}
+
+      {
+        !user 
+          && 
+        <LoginForm 
+          handleLogin={handleLogin}
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+        />
+      }
+      {user && (
+        <div>
+          <p>{user.username} loggen in</p>
+          <button 
+            onClick={() => {
+              window.localStorage.clear();
+              window.location.reload();
+            }}  
+          >
+              logout
+          </button>
+          <NoteForm 
+            addNote={addNote}
+            newNote={newNote}
+            handleNoteChange={handleNoteChange}
+          />
+        </div>
+      )}
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -83,10 +217,10 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
+      {/* <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>
+      </form> */}
 
       <Footer />
     </div>
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,33 @@
+export default function LoginForm({
+    handleLogin,
+    username,
+    setUsername,
+    password,
+    setPassword
+}) {
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          username
+          <input 
+            type="text" 
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input 
+            type="text" 
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}

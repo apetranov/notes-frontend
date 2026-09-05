42fb8a386381c8cdc26b0a2e9160d2fd9f253034
Alexander Petranov <shasho68.5@gmail.com>
2026-09-05 18:22:51 +0300

    Debugging tests

--- src/components/Note.test.jsx
+++ src/components/Note.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Note from './Note';
+import { expect } from 'vitest';
 
 test('renders content', () => {
     const note = {
@@ -9,6 +10,38 @@ test('renders content', () => {
 
     render(<Note note={note} />);
 
-    const element = screen.getByText('Component testing is done with react-testing-library');
+    screen.debug();
+
+    const element = screen.getByText(
+        'Component testing is done with react-testing-library',
+        { exact: false }
+    );
+
+    screen.debug(element);
+
     expect(element).toBeDefined();
-})
+});
+
+test('does not render this', () => {
+    const note = {
+        content: 'This is a reminder',
+        important: true 
+    };
+
+    render(<Note note={note} />);
+
+    const element = screen.queryByText('do not want this to be rendered');
+    expect(element).toBeNull();
+});
+
+test('renders content w CSS-selector querySelector', () => {
+    const note = {
+        content: 'Does not work anymore :(',
+        important: true
+    };
+
+    const { container } = render(<Note note={note} />);
+
+    const div = container.querySelector('.note');
+    expect(div).toHaveTextContent('Does not work anymore :(');
+});

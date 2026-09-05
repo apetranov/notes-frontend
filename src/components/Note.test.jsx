1cf99594b3fe2bd23edcd2750b967fe2e475cf64
Alexander Petranov <shasho68.5@gmail.com>
2026-09-05 18:28:08 +0300

    Add usr event test

--- src/components/Note.test.jsx
+++ src/components/Note.test.jsx
@@ -1,47 +1,67 @@
-import { render, screen } from '@testing-library/react';
-import Note from './Note';
-import { expect } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import Note from "./Note";
+import { expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
-test('renders content', () => {
-    const note = {
-        content: 'Component testing is done with react-testing-library',
-        important: true
-    };
+test("renders content", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
 
-    render(<Note note={note} />);
+  render(<Note note={note} />);
 
-    screen.debug();
+  screen.debug();
 
-    const element = screen.getByText(
-        'Component testing is done with react-testing-library',
-        { exact: false }
-    );
+  const element = screen.getByText(
+    "Component testing is done with react-testing-library",
+    { exact: false },
+  );
 
-    screen.debug(element);
+  screen.debug(element);
 
-    expect(element).toBeDefined();
+  expect(element).toBeDefined();
 });
 
-test('does not render this', () => {
-    const note = {
-        content: 'This is a reminder',
-        important: true 
-    };
+test("does not render this", () => {
+  const note = {
+    content: "This is a reminder",
+    important: true,
+  };
 
-    render(<Note note={note} />);
+  render(<Note note={note} />);
 
-    const element = screen.queryByText('do not want this to be rendered');
-    expect(element).toBeNull();
+  const element = screen.queryByText("do not want this to be rendered");
+  expect(element).toBeNull();
 });
 
-test('renders content w CSS-selector querySelector', () => {
-    const note = {
-        content: 'Does not work anymore :(',
-        important: true
-    };
+test("renders content w CSS-selector querySelector", () => {
+  const note = {
+    content: "Does not work anymore :(",
+    important: true,
+  };
 
-    const { container } = render(<Note note={note} />);
+  const { container } = render(<Note note={note} />);
 
-    const div = container.querySelector('.note');
-    expect(div).toHaveTextContent('Does not work anymore :(');
+  const div = container.querySelector(".note");
+  expect(div).toHaveTextContent("Does not work anymore :(");
+});
+
+test("clicking the button calls event handler once", async () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const mockHandler = vi.fn();
+
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  );
+
+  const user = userEvent.setup();
+  const button = screen.getByText('make not important');
+  await user.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
